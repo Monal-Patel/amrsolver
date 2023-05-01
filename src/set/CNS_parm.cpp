d78542c190e7c0c5fe9c5b1d3b4b6bbe1d2f9acb
Monal Patel <monal-@hotmail.com>
2023-04-30 21:16:59 -0400

    add riemann solver only in x

--- src/set/CNS_parm.cpp
+++ src/set/CNS_parm.cpp
@@ -3,7 +3,8 @@
 
 void Parm::Initialize ()
 {
-    constexpr amrex::Real Ru = amrex::Real(8.31451e7);
+    constexpr amrex::Real Ru = amrex::Real(8.31451);
     cv = Ru / (eos_mu * (eos_gamma-amrex::Real(1.0)));
     cp = eos_gamma * Ru / (eos_mu * (eos_gamma-amrex::Real(1.0)));
+    Rspec = Ru/eos_mu;
 }
--- src/tim/CNS_advance.cpp
+++ src/tim/CNS_advance.cpp
@@ -51,7 +51,6 @@ CNS::advance (Real time, Real dt, int /*iteration*/, int /*ncycle*/)
     compute_rhs(Sborder, dSdt, Real(0.5)*dt, fr_as_crse, fr_as_fine);
     // U^* = U^n + dt*dUdt^n
     MultiFab::LinComb(S_new, Real(1.0), Sborder, 0, dt, dSdt, 0, 0, NCONS, 0);
-    // computeTemp(S_new,0);
 
     // RK2 stage 2
     // After fillpatch Sborder = U^n+dt*dUdt^n
@@ -67,7 +66,6 @@ CNS::advance (Real time, Real dt, int /*iteration*/, int /*ncycle*/)
     // S_new += 0.5*dt*dSdt
     MultiFab::Saxpy(S_new, Real(0.5)*dt, dSdt, 0, 0, NCONS, 0);
     // We now have S_new = U^{n+1} = (U^n+0.5*dt*dUdt^n) + 0.5*dt*dUdt^*
-    // computeTemp(S_new,0);
 
     return dt;
 }
@@ -78,6 +76,141 @@ void CNS::compute_rhs (const MultiFab& statemf, MultiFab& dSdt, Real dt,
 {
     BL_PROFILE("CNS::compute_rhs()");
 
+if (euler_flux_type==0) {
+    const auto dx    = geom.CellSizeArray();
+    const auto dxinv = geom.InvCellSizeArray();
+    const int ncomp = NCONS;
+    const int neqns = NCONS;
+    const int ncons = NCONS;
+    const int nprim = NCONS+1;
+
+    Array<MultiFab,AMREX_SPACEDIM> fluxes;
+    for (int idim = 0; idim < AMREX_SPACEDIM; ++idim) {
+        fluxes[idim].define(amrex::convert(statemf.boxArray(),IntVect::TheDimensionVector(idim)),
+                            statemf.DistributionMap(), ncomp, 0);
+        fluxes[idim] = 0.0;
+    }
+
+    Parm const* lparm = d_parm;
+
+    FArrayBox qtmp, slopetmp;
+    for (MFIter mfi(statemf); mfi.isValid(); ++mfi)
+    {
+        const Box& bx = mfi.tilebox();
+
+        auto const& sfab = statemf.array(mfi);
+        auto const& dsdtfab = dSdt.array(mfi);
+        AMREX_D_TERM(auto const& fxfab = fluxes[0].array(mfi);,
+                     auto const& fyfab = fluxes[1].array(mfi);,
+                     auto const& fzfab = fluxes[2].array(mfi););
+
+        const Box& bxg2 = amrex::grow(bx,2);
+        qtmp.resize(bxg2, nprim);
+        Elixir qeli = qtmp.elixir();
+        auto const& q = qtmp.array();
+
+        amrex::ParallelFor(bxg2,
+        [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+        {
+            cons2prim(i, j, k, sfab, q, *lparm);
+        });
+
+        const Box& bxg1 = amrex::grow(bx,1);
+        slopetmp.resize(bxg1,neqns);
+        Elixir slopeeli = slopetmp.elixir();
+        auto const& slope = slopetmp.array();
+
+        // x-direction
+        int cdir = 0;
+        const Box& xslpbx = amrex::grow(bx, cdir, 1);
+        amrex::ParallelFor(xslpbx,
+        [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+        {
+            cns_slope_x(i, j, k, slope, q,*lparm);
+        });
+        const Box& xflxbx = amrex::surroundingNodes(bx,cdir);
+        amrex::ParallelFor(xflxbx,
+        [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+        {
+            cns_riemann_x(i, j, k, fxfab, slope, q, *lparm);
+            for (int n = neqns; n < ncons; ++n) fxfab(i,j,k,n) = Real(0.0);
+        });
+
+        // y-direction
+        // cdir = 1;
+        // const Box& yslpbx = amrex::grow(bx, cdir, 1);
+        // amrex::ParallelFor(yslpbx,
+        // [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+        // {
+        //     cns_slope_y(i, j, k, slope, q);
+        // });
+        // const Box& yflxbx = amrex::surroundingNodes(bx,cdir);
+        // amrex::ParallelFor(yflxbx,
+        // [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+        // {
+        //     cns_riemann_y(i, j, k, fyfab, slope, q, *lparm);
+        //     for (int n = neqns; n < ncons; ++n) fyfab(i,j,k,n) = Real(0.0);
+        // });
+
+        // z-direction
+        // cdir = 2;
+        // const Box& zslpbx = amrex::grow(bx, cdir, 1);
+        // amrex::ParallelFor(zslpbx,
+        // [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+        // {
+        //     cns_slope_z(i, j, k, slope, q);
+        // });
+        // const Box& zflxbx = amrex::surroundingNodes(bx,cdir);
+        // amrex::ParallelFor(zflxbx,
+        // [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+        // {
+        //     cns_riemann_z(i, j, k, fzfab, slope, q, *lparm);
+        //     for (int n = neqns; n < ncons; ++n) fzfab(i,j,k,n) = Real(0.0);
+        // });
+
+        // don't have to do this, but we could
+        qeli.clear(); // don't need them anymore
+        slopeeli.clear();
+
+        amrex::ParallelFor(bx, ncons,
+        [=] AMREX_GPU_DEVICE (int i, int j, int k, int n) noexcept
+        {
+            cns_flux_to_dudt(i, j, k, n, dsdtfab, AMREX_D_DECL(fxfab,fyfab,fzfab), dxinv);
+        });
+
+        if (gravity != Real(0.0)) {
+            const Real g = gravity;
+            const int irho = Density;
+            const int imz = Zmom;
+            const int irhoE = Eden;
+            amrex::ParallelFor(bx,
+            [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+            {
+                dsdtfab(i,j,k,imz)   += g * sfab(i,j,k,irho);
+                dsdtfab(i,j,k,irhoE) += g * sfab(i,j,k,imz);
+            });
+        }
+    }
+
+    if (fr_as_crse) {
+        for (int idim = 0; idim < AMREX_SPACEDIM; ++idim) {
+            const Real dA = (idim == 0) ? dx[1]*dx[2] : ((idim == 1) ? dx[0]*dx[2] : dx[0]*dx[1]);
+            const Real scale = -dt*dA;
+            fr_as_crse->CrseInit(fluxes[idim], idim, 0, 0, NCONS, scale, FluxRegister::ADD);
+        }
+    }
+
+    if (fr_as_fine) {
+        for (int idim = 0; idim < AMREX_SPACEDIM; ++idim) {
+            const Real dA = (idim == 0) ? dx[1]*dx[2] : ((idim == 1) ? dx[0]*dx[2] : dx[0]*dx[1]);
+            const Real scale = dt*dA;
+            fr_as_fine->FineAdd(fluxes[idim], idim, 0, 0, NCONS, scale);
+        }
+    }
+
+}
+
+else {
     const auto dx = geom.CellSizeArray();
     const auto dxinv = geom.InvCellSizeArray();
 
@@ -176,6 +309,7 @@ void CNS::compute_rhs (const MultiFab& statemf, MultiFab& dSdt, Real dt,
             fr_as_fine->FineAdd(numflxmf[idim], idim, 0, 0, NCONS, scale);
         }
     }
+}
 }
 
 // if (gravity != Real(0.0)) {
