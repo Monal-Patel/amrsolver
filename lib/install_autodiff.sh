49fd3bdb906cb562cf43c22255d4550e18158a45
Monal Patel <monal-@hotmail.com>
2024-01-18 09:53:05 -0500

    update libs install scripts

--- lib/install_autodiff.sh
+++ lib/install_autodiff.sh
@@ -1,4 +1,5 @@
 cd autodiff
+rm -rf .build
 mkdir .build && cd .build
-cmake ..
-cmake --build . --target install
+cmake .. -DCMAKE_PREFIX_PATH=/home/monal/.local/lib/python3.10/site-packages -DAUTODIFF_BUILD_TESTS=OFF -DCMAKE_INSTALL_PREFIX=$AMR_SOLVER/lib/install/autodiff
+cmake --build . --target install -j 4
--- lib/install_googletest.sh
+++ lib/install_googletest.sh
@@ -1,5 +1,5 @@
 cd googletest        # Main directory of the cloned repository.
 mkdir build          # Create a directory to hold the build output.
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=$AMR_SOLVER/lib/install/googletest ..             # Generate native build scripts for GoogleTest.
+cmake -Dgtest_build_samples=ON -DCMAKE_INSTALL_PREFIX=$AMREX_HOME/../install/googletest ..             # Generate native build scripts for GoogleTest.
 make install
